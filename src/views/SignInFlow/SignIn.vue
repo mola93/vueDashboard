b880d91d1683a25c898209a3a56aad713ab42984
mola <molamin73@gmail.com>
2019-10-11 16:45:28 +0000

    added light/dark view

--- src/views/SignInFlow/SignIn.vue
+++ src/views/SignInFlow/SignIn.vue
@@ -1,25 +1,104 @@
 <template>
-  <div class="container">
-    <div class="request">
+  <div
+    class="container"
+    :class="{ 'light-background': !isDarkMode, 'dark-background': isDarkMode }"
+  >
+    <div
+      class="request"
+      :class="{ 'light-request': isDarkMode, 'dark-request': !isDarkMode }"
+    >
       Don't have a Design+Code HQ account?
       <router-link to="/request">Request an account</router-link>
     </div>
     <div class="login">
       <img src="@/assets/logo.png" />
-      <h4>Sign into Design HQ</h4>
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
+      <h4 :class="{ 'light-text': isDarkMode, 'dark-text': !isDarkMode }">
+        Sign into Design HQ
+      </h4>
+      <input
+        type="email"
+        placeholder="Email"
+        :class="{ 'light-field': isDarkMode, 'dark-field': !isDarkMode }"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        :class="{ 'light-field': isDarkMode, 'dark-field': !isDarkMode }"
+      />
       <button>Sign In</button>
-      <router-link to="/recover">Forgot your password?</router-link>
+      <router-link
+        to="/recover"
+        :class="{ 'light-link': isDarkMode, 'dark-link': !isDarkMode }"
+        >Forgot your password?</router-link
+      >
+      <button @click="toggleDarkMode">Toggle</button>
     </div>
   </div>
 </template>
 
 <script>
-export default { name: "SignIn" };
+export default {
+  name: "SignIn",
+  data: function() {
+    return { isDarkMode: true };
+  },
+  methods: {
+    toggleDarkMode() {
+      this.isDarkMode = !this.isDarkMode;
+      document.body.style.background = this.isDarkMode ? "#212c4f" : "#f0f3f5";
+    }
+  }
+};
 </script>
 
 <style scoped lang="scss">
+.light-text {
+  color: $white;
+}
+
+.dark-text {
+  color: $black;
+}
+
+.light-field {
+  background: rgba(255, 255, 255, 0.2);
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.3);
+  }
+}
+
+.dark-field {
+  background: rgba(198, 208, 235, 0.2);
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.3);
+  }
+}
+
+.light-request {
+  color: rgba(255, 255, 255, 0.3);
+
+  a {
+    color: white;
+  }
+}
+
+.dark-request {
+  color: rgba(0, 0, 0, 0.3);
+
+  a {
+    color: $black;
+  }
+}
+
+.light-background {
+  background-color: $light-gray;
+}
+.dark-background {
+  background-color: $dark-blue;
+}
+
 .container {
   display: flex;
   justify-content: center;
